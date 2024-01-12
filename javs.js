2012a9870e983c241f6794b805188e7fa9befede
Rahmatullah Zadran <rahmatullahzadran@outlook.com>
2024-01-12 21:27:49 +0000

    updated JS buttons

---
+++ javs.js
@@ -0,0 +1,8 @@
+var start = document.getElementById("start");
+var highscore = document.getElementById("highscore");
+
+
+start.addEventListener("click", function(){
+
+    window.location.href = "quiz.html";
+})
